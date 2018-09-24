4dc7095e67e4307d8a93da7ed6d608f4fb1b0c50
newLana <newLana@users.noreply.github.com>
2018-09-24 11:51:02 +0300

    Add project files.

---
+++ ConsoleApp2/GetterOfSystemTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class GetterOfSystemTree
+    {
+        public string UsersDocsPath => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public string ResFileName => Path.GetFileName(usersDesktopPath);
+
+        private string usersDesktopPath;
+        private string tree;
+        private StringBuilder sb;
+
+        public GetterOfSystemTree(string symbol)
+        {
+            usersDesktopPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Environment.UserName + "Docs.txt");
+            sb = new StringBuilder();
+            GetTree(new DirectoryInfo(UsersDocsPath), sb, symbol);
+            tree = sb.ToString();
+        }       
+
+        private void GetTree(DirectoryInfo directoryInfo, StringBuilder sb, string shift)
+        {
+            if(directoryInfo != null)
+            {
+                var checkTime = DateTime.Now.Subtract(new TimeSpan(15, 0, 0, 0));
+                var allFileSystemInfos = directoryInfo.EnumerateFileSystemInfos().Where(f => (!f.Attributes.HasFlag(FileAttributes.ReparsePoint)) && (f.CreationTime < checkTime));
+                if (allFileSystemInfos != null)
+                {
+                    foreach (var item in allFileSystemInfos)
+                    {
+                        sb.Append(shift+item.Name + Environment.NewLine);
+                        if(item is DirectoryInfo)
+                        {
+                            GetTree(item as DirectoryInfo, sb, shift + shift[0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool WriteTree()
+        {
+            bool isWrited = false;
+            if(!string.IsNullOrEmpty(tree))
+            {                
+                using (StreamWriter fi = new StreamWriter(usersDesktopPath, false, Encoding.Default))
+                {
+                    if (!File.Exists(usersDesktopPath))
+                        File.Create(usersDesktopPath);
+                    fi.Write(tree);
+                }
+                isWrited = true;
+            }
+            return isWrited;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ разделителя");
+            var symbol = Console.ReadLine()[0].ToString();
+            var treeGetter = new GetterOfSystemTree(symbol);
+            
+            if(treeGetter.WriteTree())
+                Console.WriteLine($"Дерево успешно записано в файл {treeGetter.ResFileName} на рабочем столе.");
+
+            Console.ReadKey();
+        }
+    }
+}
